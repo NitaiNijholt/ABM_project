dfa8ff1238dddc74ac3b4b12ea33e14fab3270b1
KLCALAW <57180289+KLCALAW@users.noreply.github.com>
2024-06-12 17:08:09 +0200

    Orderbook done. Make other files compatible now

--- orderbook.py
+++ orderbook.py
@@ -1,29 +1,39 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import random
 
+# Make buying / selling price based on EV of the building of the houses?
 
-# In[ ]:
-
-
-class OrderBook:
+class OrderBooks:
     def __init__(self, agents_dict):
         self.bids = []
         self.asks = []
+        self.transactions = []
         self.agents_dict = agents_dict
     
     def place_bid(self, agent_id, price):
         self.bids.append({'agent_id': agent_id, 'price': price})
+        self.match_orders('bid')  # Pass 'bid' to indicate the function is called from place_bid
         
     def place_ask(self, agent_id, price):
         self.asks.append({'agent_id': agent_id, 'price': price})
+        self.match_orders('ask')  # Pass 'ask' to indicate the function is called from place_ask
         
-    def match_orders(self):
+    def check_price(self):
+        # Either there are orders left in the bids/ask or none
+        if self.bids:
+            best_bid = max(self.bids, key=lambda x: x['price'])
+        else:
+            best_bid = None
+        
+        if self.asks:
+            best_ask = min(self.asks, key=lambda x: x['price'])
+        else:
+            best_ask = None
+        return best_bid, best_ask
+        
+    def match_orders(self, order_type):
         transactions = []
+        # Check for matching orders immediately when a new order is placed
+        
         while self.bids and self.asks:
             best_bid = max(self.bids, key=lambda x: x['price'])
             best_ask = min(self.asks, key=lambda x: x['price'])
@@ -32,7 +42,11 @@ class OrderBook:
                 bid_agent = self.agents_dict[best_bid['agent_id']]
                 ask_agent = self.agents_dict[best_ask['agent_id']]
                 
-                price = best_ask['price']
+                # Use the existing order's price for the transaction
+                if order_type == 'bid':
+                    price = best_ask['price']
+                else:
+                    price = best_bid['price']
                 
                 bid_agent['wealth'] -= price
                 bid_agent['wood'] += 1  
@@ -43,31 +57,6 @@ class OrderBook:
                 self.bids.remove(best_bid)
                 self.asks.remove(best_ask)
                 
-                transactions.append({'buyer': bid_agent, 'seller': ask_agent, 'price': price})
+                self.transactions.append({'buyer': best_bid['agent_id'], 'seller': best_ask['agent_id'], 'price': price})
             else:
                 break
-        
-        return transactions
-    
-    def set_aggregate_price(self, prev_price):
-        self.bids.sort(key=lambda x: x['price'], reverse=True)
-        self.asks.sort(key=lambda x: x['price'])
-        
-        if not self.bids or not self.asks:
-            return prev_price
-        
-        best_bid = self.bids[0]['price']
-        best_ask = self.asks[0]['price']
-        
-        if best_bid >= best_ask:
-            transactions = self.match_orders()
-            avg_price = sum(t['price'] for t in transactions) / len(transactions)
-            new_price = avg_price + self.delta * len(transactions)
-        else:
-            new_price = prev_price
-        
-        self.bids = []
-        self.asks = []
-        
-        return new_price
-
