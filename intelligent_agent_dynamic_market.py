dbf82935f4b5f82dfb5892cea2a2cf75722dd854
KLCALAW <ichinocalvin@hotmail.nl>
2024-07-10 14:03:07 +0200

    docstrings, remove comments & redundant code

--- intelligent_agent_dynamic_market.py
+++ intelligent_agent_dynamic_market.py
@@ -11,7 +11,7 @@ class IntelligentAgent(Agent):
     def __init__(self, sim, agent_id, position, grid, market, creation_time, 
                  wealth=0, wood=0, stone=0, income_per_timestep=1, network=None):
         """
-        Initialize agent with given position, wealth, wood, stone, grid, market, life expectancy, and creation time.
+        Initialize agent with given position, wealth, wood, stone, grid, market, and creation time.
 
         Parameters
         ----------
@@ -27,24 +27,19 @@ class IntelligentAgent(Agent):
             Market object where the agent can trade resources.
         creation_time : int
             Time when the agent was created.
-        wealth : int
-            Initial wealth of the agent.
-        wood : int
-            Initial number of the wood resources of the agent.
-        stone : int
-            Initial number of the stone resources of the agent.
-        lifetime_distribution : str
-            Distribution of the lifetime.
-            Options: 'gamma', 'lognormal'.
-        lifetime_mean : float
-            Mean of the lifetime distribution.
-        lifetime_std : float
-            Standard deviation of the lifetime distribution.
+        wealth : int, optional
+            Initial wealth of the agent (default is 0).
+        wood : int, optional
+            Initial number of the wood resources of the agent (default is 0).
+        stone : int, optional
+            Initial number of the stone resources of the agent (default is 0).
+        income_per_timestep : int, optional
+            Income per timestep for the agent (default is 1).
+        network : np.ndarray, optional
+            Pre-trained neural network parameters (default is None).
         """
-
         super().__init__(sim, agent_id, position, grid, market, creation_time, 
                  wealth, wood, stone, income_per_timestep=income_per_timestep)
-
 
         self.fitness = wealth + stone * market.stone_rate + wood * market.wood_rate
 
@@ -71,7 +66,6 @@ class IntelligentAgent(Agent):
             self.primary_output_size  # Biases for the output layer
         )
 
-
         if network is None:
             self.network = np.random.rand(total_parameters)
         else:
@@ -85,6 +79,14 @@ class IntelligentAgent(Agent):
         self.last_order_time = -self.order_cooldown_period
         
     def build_model(self):
+        """
+        Build the neural network model for the agent.
+
+        Returns
+        -------
+        Network
+            A neural network model for the agent.
+        """
         model = Network(self.input_size,
                         self.hidden_size,
                         self.primary_output_size,
@@ -93,8 +95,15 @@ class IntelligentAgent(Agent):
     
         return model
     
-    
     def get_inputs(self):
+        """
+        Get the input features for the neural network model.
+
+        Returns
+        -------
+        np.ndarray
+            An array of input features for the neural network.
+        """
         initial_inputs = np.array([
             np.log1p(self.market.wood_rate),
             np.log1p(self.market.stone_rate),
@@ -138,11 +147,12 @@ class IntelligentAgent(Agent):
         # Combine all inputs into a single array
         inputs = np.concatenate([initial_inputs, neighbor_inputs, current_cell_inputs])
 
-
         return inputs
 
-
     def update_fitness(self):
+        """
+        Update the fitness of the agent based on its wealth and initial wealth.
+        """
         fitness = np.sum([
             self.wealth - self.initial_wealth, 
         ])
@@ -153,39 +163,41 @@ class IntelligentAgent(Agent):
         """
         Agent moves to the specified direction if the target cell is empty.
         If all neighboring cells are occupied, the agent does not move.
+
+        Parameters
+        ----------
+        direction : str
+            Direction to move ('up', 'down', 'left', 'right', 'stay').
         """
         x, y = self.position
         if direction == 'up':
-            new_position = (x, (y - 1)%self.grid.height)
+            new_position = (x, (y - 1) % self.grid.height)
             self.current_action = 'move'
         elif direction == 'down':
-            new_position = (x, (y + 1)%self.grid.height)
+            new_position = (x, (y + 1) % self.grid.height)
             self.current_action = 'move'
         elif direction == 'left':
-            new_position = ((x - 1)%self.grid.width, y)
+            new_position = ((x - 1) % self.grid.width, y)
             self.current_action = 'move'
         elif direction == 'right':
-            new_position = ((x + 1)%self.grid.width, y)
+            new_position = ((x + 1) % self.grid.width, y)
             self.current_action = 'move'
         elif direction == 'stay':
             new_position = self.position
         else:
             raise ValueError("Invalid direction")
 
-
         # Ensure new position is within grid bounds
-        if self.grid.agent_matrix[new_position] == 0 or new_position==self.position:
+        if self.grid.agent_matrix[new_position] == 0 or new_position == self.position:
             # Move the agent
             self.grid.agent_matrix[self.position] = 0
             self.grid.agent_matrix[new_position] = self.agent_id
             self.position = new_position
-            # self.sim.moving += 1
 
     def gather(self):
         """
         Agent gathers resources from the current position.
         """
-
         if self.grid.resource_matrix_wood[self.position] > 0:
             self.wood += 1
             self.grid.resource_matrix_wood[self.position] -= 1
@@ -199,16 +211,13 @@ class IntelligentAgent(Agent):
             self.gathered_at_timesteps.append(1)
             self.current_action = 'gather'
             self.sim.gathering += 1
-        
-        
-        self.gathered_at_timesteps.append(0)
 
+        self.gathered_at_timesteps.append(0)
 
     def build(self):
         """
         Agent builds a house at the current position if the agent has enough resources and the cell is empty.
         """
-
         # Only build if the agent has the resources to do so
         wood_cost, stone_cost = self.grid.house_cost
         if self.wood >= wood_cost and self.stone >= stone_cost and self.grid.house_matrix[self.position] < self.grid.max_house_num:
@@ -218,8 +227,13 @@ class IntelligentAgent(Agent):
             self.current_action = 'start_building'
             self.sim.build += 1
 
-
     def step(self):
+        """
+        Execute one step in the simulation for this agent.
+
+        This method handles the decision-making process for the agent,
+        determining and performing the best action based on the neural network model.
+        """
         if self.currently_building_timesteps > 0:
             self.currently_building_timesteps += 1
             if self.currently_building_timesteps == self.required_building_time:
@@ -233,7 +247,6 @@ class IntelligentAgent(Agent):
             # Sort actions based on output values in descending order
             sorted_actions = np.argsort(outputs[0])[::-1]
 
-            
             # Iterate through actions in the order of their desirability
             for action in sorted_actions:
                 if action == 0:
@@ -252,7 +265,7 @@ class IntelligentAgent(Agent):
                         if self.buy():
                             break
                 elif action == 8:
-                    if self.can_place_order() and self.wood > 0 or self.stone > 0:
+                    if self.can_place_order() and (self.wood > 0 or self.stone > 0):
                         self.sell()
                         break
                 else:
@@ -272,46 +285,35 @@ class IntelligentAgent(Agent):
                     elif action == 6:
                         self.move('stay')
                         break
-                
 
-            # actions[action]()
-            # print(f"Agent {self.agent_id} does {self.current_action}, ({action})")
-        
         self.collect_income()
         self.wealth_over_time.append(self.wealth)
         self.houses_over_time.append(len(self.houses))
         self.bought_at_timesteps.append(0)
         self.sold_at_timesteps.append(0)
         self.taxes_paid_at_timesteps.append(0)
-    
 
     def die(self):
-        # if self.fitness > 0:
-        #     if self.sim.writer:
-        #         consolidated_data = (
-        #             self.wealth_over_time +
-        #             self.houses_over_time +
-        #             self.gathered_at_timesteps +
-        #             self.bought_at_timesteps +
-        #             self.sold_at_timesteps +
-        #             self.taxes_paid_at_timesteps
-        #         )
-        #         self.sim.writer.writerow(np.concatenate(([self.fitness], np.array(self.network))))
-
+        """
+        Handle the death of the agent, removing it from the simulation.
+        """
         del self.grid.agents[self.agent_id]
         del self.sim.agent_dict[self.agent_id]
         self.grid.agent_matrix[self.position] = 0
 
         for house in self.houses:
             self.grid.house_matrix[house.position] -= 1
-            
-
 
     def buy(self):
         """
         Agent buys resources from the market.
+        
+        Returns
+        -------
+        bool
+            True if the buy action was successful, False otherwise.
         """
-        # Only buy if agent has enough wealth to affort it
+        # Only buy if agent has enough wealth to afford it
         wood_to_buy = max(0, self.grid.house_cost[0] - self.wood)
         stone_to_buy = max(0, self.grid.house_cost[1] - self.stone)
         determined_price_wood = self.determine_price('buy', 'wood')
@@ -321,35 +323,37 @@ class IntelligentAgent(Agent):
             wood_price = min(determined_price_wood, self.wealth * wood_to_buy / (wood_to_buy + stone_to_buy))
             stone_price = min(determined_price_stone, self.wealth * stone_to_buy / (wood_to_buy + stone_to_buy))
 
-            if self.wealth >= (wood_to_buy * wood_price + stone_to_buy * stone_price) and (wood_to_buy > 0 or stone_to_buy > 0 ):
+            if self.wealth >= (wood_to_buy * wood_price + stone_to_buy * stone_price) and (wood_to_buy > 0 or stone_to_buy > 0):
                 self.current_action = 'buy'
 
-                self.place_order(self.order_books, 'wood', 'buy', price = wood_price, quantity = wood_to_buy)
-                self.place_order(self.order_books, 'stone', 'buy', price = stone_price, quantity = stone_to_buy)
+                self.place_order(self.order_books, 'wood', 'buy', price=wood_price, quantity=wood_to_buy)
+                self.place_order(self.order_books, 'stone', 'buy', price=stone_price, quantity=stone_to_buy)
 
                 self.sim.buy += 1
                 return True
         return False
 
-
     def sell(self):
         """
         Agent sells all resources to the market.
         """
-        
         if self.wood > 0 or self.stone > 0:
             wood_price = self.determine_price('sell', 'wood')
             stone_price = self.determine_price('sell', 'stone')
             if wood_price and stone_price:
                 self.current_action = 'sell'
                 self.sim.sell += 1
-                self.place_order(self.order_books, 'wood', 'sell', price = wood_price, quantity = self.wood)
-                self.place_order(self.order_books, 'stone', 'sell', price = stone_price, quantity = self.stone)
-
+                self.place_order(self.order_books, 'wood', 'sell', price=wood_price, quantity=self.wood)
+                self.place_order(self.order_books, 'stone', 'sell', price=stone_price, quantity=self.stone)
 
     def can_place_order(self):
         """
         Check if the agent can place an order based on the order limit and cooldown period.
+
+        Returns
+        -------
+        bool
+            True if the agent can place an order, False otherwise.
         """
         current_timestep = self.sim.t
         if self.orders_placed < self.order_limit:
@@ -377,7 +381,6 @@ class IntelligentAgent(Agent):
         quantity : int
             The amount of the resource to trade.
         """
-
         if resource_type not in order_books:
             raise ValueError(f"Invalid resource type: {resource_type}")
 
@@ -395,18 +398,28 @@ class IntelligentAgent(Agent):
         self.orders_placed += 1
         self.last_order_time = self.sim.t
 
-        # # Log the order placement for debugging
-        # print(f"Agent {self.agent_id} placed a {order_type} order for {quantity} units of {resource_type} at price {price}.")
-
-
-        
     def determine_price(self, order_type, resource):
+        """
+        Determine the price for buying or selling a resource.
+
+        Parameters
+        ----------
+        order_type : str
+            The type of order to place ('buy' or 'sell').
+        resource : str
+            The type of resource to trade ('wood' or 'stone').
+
+        Returns
+        -------
+        float or None
+            The determined price for the resource, or None if the total income is zero.
+        """
         age = self.sim.t - self.creation_time
         total_income = max(self.income_per_timestep * (self.guessed_lifetime - age - self.required_building_time), 0)
         if total_income == 0:
             return None
         earning_rate = total_income / self.required_building_time
-        m_price = earning_rate / sum(self.grid.house_cost) #* 10**(np.random.normal(0, 0.01)), 1)
+        m_price = earning_rate / sum(self.grid.house_cost)
 
         ob_price_wood_bid, ob_price_wood_ask = self.sim.wood_order_book.check_price()
         ob_price_stone_bid, ob_price_stone_ask = self.sim.stone_order_book.check_price()
@@ -434,3 +447,4 @@ class IntelligentAgent(Agent):
                 self.market.stone_rate = m_price if resource == 'stone' else self.market.wood_rate
 
         return self.market.wood_rate if resource == 'wood' else self.market.stone_rate
+
